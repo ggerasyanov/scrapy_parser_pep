60152c6c325e6145881f716d1ef9e320a81cea9c
ggerasyanov <QieDie@yandex.ru>
2022-07-15 11:53:47 +0300

    done parser

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,13 +1,44 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import csv
+import datetime as dt
+from collections import defaultdict
+from pathlib import Path
 
-
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+BASE_DIR = Path(__file__).parent.parent
+DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
 
 
 class PepParsePipeline:
+
+    count_status = defaultdict(int)
+    total = 0
+    result = []
+
+    def open_spider(self, spider):
+        pass
+
     def process_item(self, item, spider):
+        self.count_status[item['status']] += 1
+        self.total += 1
         return item
+
+    def close_spider(self, spider):
+        self.result.append(('Статус', 'Количество'))
+        for status, count in self.count_status.items():
+            self.result.append(
+                (status, count)
+            )
+
+        self.result.append(
+            ('Total', self.total)
+        )
+
+        now = dt.datetime.now()
+        now_formatted = now.strftime(DATETIME_FORMAT)
+
+        with open(
+            BASE_DIR / 'results' / f'status_summary_{now_formatted}.csv',
+            'w',
+            encoding='utf-8'
+        ) as f:
+            write = csv.writer(f, dialect='unix')
+            write.writerows(self.result)
--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,10 +1,29 @@
 import scrapy
 
+from pep_parse.items import PepParseItem
+
 
 class PepSpider(scrapy.Spider):
     name = 'pep'
     allowed_domains = ['peps.python.org']
-    start_urls = ['http://peps.python.org/']
+    start_urls = ['https://peps.python.org/']
 
     def parse(self, response):
-        pass
+        pep_links = response.css('a[href^="/pep-"]')
+
+        for link in pep_links:
+            yield response.follow(link, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        title = response.css('h1.page-title::text').get()
+        table = response.css('dl.rfc2822')
+        number = title.split()[1]
+        name = ' '.join(title.split()[3::1])
+        status = table.css('dt:contains("Status") + dd::text').get()
+
+        data = {
+            'number': number,
+            'name': name,
+            'status': status,
+        }
+        yield PepParseItem(data)
